ed14ccc159890859e0beb4bbdccdebf62ca69a08
RossieK <rosi.kirova96@gmail.com>
2021-02-13 17:23:06 +0200

    Add home controllers and routes file

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const { port } = require('./config/config');
 const express = require('express');
 const expressConfig = require('./config/express');
+const routes = require('./routes');
 
 //Initialize App
 const app = express();
 expressConfig(app);
 
 //Routes
-app.get('/', (req, res) => {
-    res.render('home');
-})
+app.use(routes);
 
 //Server initialization
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
---
+++ routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const homeController = require('./controllers/homeController');
+
+const router = Router();
+
+router.use('/', homeController);
+
+module.exports = router;
