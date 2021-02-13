a48789b62f778f2d16c895c0a2cbdfd373b3b2fb
RossieK <rosi.kirova96@gmail.com>
2021-02-13 19:03:02 +0200

    Combine home views

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('guestHome', { title: 'Theaters App' });
+    res.render('home', { title: 'Theaters App' });
 });
 
 module.exports = router;
