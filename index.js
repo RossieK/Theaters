1a9c7f061f8b28254240aa9f785da358c2464059
RossieK <rosi.kirova96@gmail.com>
2021-02-13 17:56:32 +0200

    Initiliaze mongoose

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { port } = require('./config/config');
 const express = require('express');
 const expressConfig = require('./config/express');
+const mongooseConfig = require('./config/mongoose');
 const routes = require('./routes');
 
 //Initialize App
 const app = express();
 expressConfig(app);
+mongooseConfig(app);
 
 //Routes
 app.use(routes);
