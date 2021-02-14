537f7d7d54026640fbf37bdc1de442c497bf3fce
RossieK <rosi.kirova96@gmail.com>
2021-02-14 16:41:23 +0200

    Add sort by likes functionality

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -40,4 +40,16 @@ router.get('/sortByDate', isLogged, (req, res) => {
         });
 });
 
+router.get('/sortByLikes', isLogged, (req, res) => {
+    playService.getAll()
+        .then(plays => {
+            plays.sort((a, b) => b.usersLiked.length - a.usersLiked.length);
+            res.render('home', { title: 'Theaters App', plays });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json('Something went wrong on our side. We\'re sorry!');
+        });
+});
+
 module.exports = router;
