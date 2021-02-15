16bf3daa13c5563a1073300d854658140cc9486e
RossieK <rosi.kirova96@gmail.com>
2021-02-15 22:02:26 +0200

    Fix warnings

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 module.exports = (app) => {
     mongoose.connect('mongodb://localhost/Theaters', { useNewUrlParser: true, useUnifiedTopology: true });
 
+    mongoose.set('useCreateIndex', true);
+
     const db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'connection error:'));
